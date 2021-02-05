7e13b720fc1cc8a7486bf17fc1e26e0cc9ba70e0
Maya Reich <maya.reich@viasat.com>
2021-02-04 22:21:01 -0800

    Made contact page

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ function App() {
           <Route path="/portfolio" component={Portfolio} />
           <Route path="/contact" component={Contact} />
         </Switch>
-        <AboutMe />
+        {/* <AboutMe /> */}
       </div>
     </Router>
   );
--- src/components/NavBar/Tabs/Contact.tsx
+++ src/components/NavBar/Tabs/Contact.tsx
@@ -1,7 +1,71 @@
 import React, { Component, useState } from 'react';
+import Text from './../../Text';
+import TextField from '@material-ui/core/TextField';
+import { rgbToHex } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
 const Contact = (): JSX.Element => {
-  return <h1>Contact</h1>;
+  const useStyles = makeStyles((theme: any) => ({
+    root: {
+      background: 'black',
+    },
+    input: {
+      background: 'rgb(24, 24, 24)',
+      color: 'white',
+    },
+  }));
+  const classes = useStyles();
+  return (
+    <div style={{ minHeight: 1000, paddingTop: 150 }}>
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          textAlign: 'center',
+          flexDirection: 'column',
+        }}
+      >
+        <Text large>{`Let's chat!`}</Text>
+        <Text small>{`Drop me a line if you have any questions or want to work together.`}</Text>
+        {/* <div style={{ backgroundColor: 'white' }}> */}
+        <div style={{ marginTop: 30 }}>
+          <TextField
+            id="filled-basic"
+            variant="outlined"
+            InputProps={{ className: classes.input }}
+            className={classes.root}
+            style={{ width: 700 }}
+            defaultValue="Name"
+          />
+        </div>
+        <div style={{ marginTop: 10 }}>
+          <TextField
+            id="filled-basic"
+            variant="outlined"
+            InputProps={{ className: classes.input }}
+            className={classes.root}
+            style={{ width: 700 }}
+            defaultValue="Email"
+          />
+        </div>
+        <div style={{ marginTop: 10 }}>
+          <TextField
+            id="outlined-multiline-static"
+            multiline
+            rows={7}
+            variant="outlined"
+            InputProps={{ className: classes.input }}
+            className={classes.root}
+            style={{ width: 700 }}
+            defaultValue="Your message"
+          />
+        </div>
+
+        {/* </div> */}
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
